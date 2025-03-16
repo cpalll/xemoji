9f827b504d28889057ebde728b9c4681b85fef46
cpal <alvinyan819@gmail.com>
2025-03-16 20:59:22 +1000

    Preprocess tweet texts

--- xemoji.py
+++ xemoji.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import numpy as np
+import re
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
 
 # Set display options
 pd.set_option('display.max_columns', None)  # Show all columns
@@ -10,13 +15,25 @@ pd.set_option('display.max_colwidth', 50)   # Limit column width
 tweets = pd.read_csv('twitter_sentiment.csv', encoding='latin-1')
 tweets.columns = ['text', 'sentiment']  # Rename columns
 
-# Add index column
+#Add index column
 tweets['index'] = range(0, len(tweets))
-tweets.set_index('index')
-# Remove all columns except for sentiment and text
-tweets = tweets[['index', 'sentiment', 'text']]
+tweets.set_index('index', inplace=True)
 
-tweets['sentiment_emoji'] = tweets['sentiment'].map({-1: 😠, 0: 😐, 1: 😀})
+# Remove all columns except for sentiment and text
+tweets = tweets[['sentiment', 'text']]
+#Assign emoji to corresponding sentiment value
+tweets['sentiment_emoji'] = tweets['sentiment'].map({-1: '😠', 0: '😐', 1: '😀'})
+
+#Preprocess the text
+tweets['text'] = tweets['text'].str.lower()
+#Remove missing value rows
+tweets.dropna(inplace=True)
+#Remove punctuation
+tweets['text'] = tweets['text'].apply(lambda x: re.sub(r'[^\w\s]', '', x))
+
+
+stop_words = set(stopwords.words('english'))
+tweets['text'] = tweets['text'].apply(lambda x: ' '.join([word for word in x.split() if word not in stop_words]))
 
 print(tweets.head())
 
