bb39c2edcde0a7cf5cb51f9243e01e7065c0bfa5
cpal <alvinyan819@gmail.com>
2025-03-16 22:35:02 +1000

    Train logistic regression model and evaluate model's accuracy

--- xemoji.py
+++ xemoji.py
@@ -6,12 +6,14 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import joblib
 
 
 
-
 nltk.download('stopwords')
+nltk.download('punkt_tab')
 
 # Set display options
 pd.set_option('display.max_columns', None)  # Show all columns
@@ -37,8 +39,7 @@ tweets['text'] = tweets['text'].str.lower()
 tweets.dropna(inplace=True)
 #Remove punctuation
 tweets['text'] = tweets['text'].apply(lambda x: re.sub(r'[^\w\s]', '', x))
-
-
+#Remove stopwords
 stop_words = set(stopwords.words('english'))
 tweets['text'] = tweets['text'].apply(lambda x: ' '.join([word for word in x.split() if word not in stop_words]))
 #Tokenize text
@@ -49,15 +50,30 @@ tweets['text'] = tweets['text'].apply(word_tokenize)
 tweets['text'] = tweets['text'].apply(lambda x: ' '.join(x))
 vectorizer = TfidfVectorizer(max_features=5000)
 X = vectorizer.fit_transform(tweets['text'])
-
 # Split the data
 X_train, X_test, y_train, y_test = train_test_split(X, tweets['sentiment_emoji'],
                                                     test_size=0.2, random_state=42)
-
 # Save the vectorizer and processed data
 joblib.dump(vectorizer, 'tfidf_vectorizer.pkl')
 joblib.dump((X_train, X_test, y_train, y_test), 'processed_data.pkl')
 
+#Train logistic regression model
+model = LogisticRegression( solver='lbfgs', max_iter=1000)
+model.fit(X_train, y_train)
 
-print(tweets.head())
+#Evaluate model
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy * 100:.2f}%")
+print("Classification Report:")
+print(classification_report(y_test, y_pred))
+conf_matrix = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix:")
+print(conf_matrix)
+
+# Save the model
+joblib.dump(model, 'logistic_regression_model.pkl')
+
+# Save the vectorizer
+joblib.dump(vectorizer, 'tfidf_vectorizer.pkl')
 
